809d6357bfd6234d1afd60fe504c73590b20825f
P4t4m8n <eranopenuni@gmail.com>
2024-03-12 14:43:52 +0200

    create color modal

---
+++ src/app/cmps/color-modal/color-modal.component.ts
@@ -0,0 +1,39 @@
+import { Component, ElementRef, HostListener, inject } from '@angular/core';
+
+@Component({
+  selector: 'color-modal',
+  templateUrl: './color-modal.component.html',
+  styleUrl: './color-modal.component.scss'
+})
+export class ColorModalComponent {
+
+  colors: string[] = [
+    '#ffffff', '#f28b82', '#fbbc04', '#fff475',
+    '#ccff90', '#a7ffeb', '#cbf0f8', '#aecbfa',
+    '#d7aefb', '#fdcfe8', '#e6c9a8', '#e8eaed']
+
+  elRef = inject(ElementRef)
+  isOpen: boolean = false
+
+
+  selectColor(color: string): void {
+    console.log("color:", color)
+    this.handleModal(false)
+
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleColorClick(event: MouseEvent) {
+    event.preventDefault()
+    event.stopPropagation()
+
+    if (!this.elRef.nativeElement.contains(event.target)) {
+      this.handleModal(false)
+    }
+  }
+
+  handleModal(isOpen: boolean): void {
+    this.isOpen = isOpen
+  }
+
+}
