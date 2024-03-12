ab9853afd4c7d4d4e596c7cecbd775c47d6000d7
P4t4m8n <eranopenuni@gmail.com>
2024-03-12 14:44:34 +0200

    Add util service

---
+++ src/app/services/util.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilService {
+
+  constructor() { }
+
+  static makeId = (length = 16) => {
+    var txt = ''
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+    for (var i = 0; i < length; i++) {
+      txt += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+
+    return txt
+  }
+
+  static getRandomColor = () => {
+    var letters = '0123456789ABCDEF'
+    var color = '#'
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)]
+    }
+    return color
+  }
+}
